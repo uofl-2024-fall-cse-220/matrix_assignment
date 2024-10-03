59dee7eefb3c58ec9f52a94a252c0e37670cfeb6
Nathan Russell <nathan@blackpathway.com>
2024-10-03 15:35:43 -0400

    Added comments to the interface

--- MatrixAssignment/src/main/java/io/github/nathanjrussell/MatrixInterface.java
+++ MatrixAssignment/src/main/java/io/github/nathanjrussell/MatrixInterface.java
@@ -1,10 +1,10 @@
 package io.github.nathanjrussell;
 
 public interface MatrixInterface {
-    void setElement(int row, int col, int value);
-    int getElement(int row, int col);
-    void doubleEveryEntry();
-    void addOneToEveryEntry();
-    int getNumRows();
-    int getNumCols();
+    void setElement(int row, int col, int value); //This method will set/update a value in a specific row nad column
+    int getElement(int row, int col); //This method will get the value in a specific row and column
+    void doubleEveryEntry(); //This method will double every entry in the matrix
+    void addOneToEveryEntry(); //This method will add one to every entry in the matrix
+    int getNumRows(); //This method will return the number of rows  in the matrix
+    int getNumCols(); //This method will return the number of columns in the matrix
 }
